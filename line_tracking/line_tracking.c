cadcbd0b2f1b01e4549e943a92bc9af652baa7e2
DongHyeon Soon <dhsoon@dgist.ac.kr>
2024-06-05 22:20:36 +0900

    Update line_tracking.c

--- line_tracking/line_tracking.c
+++ line_tracking/line_tracking.c
@@ -1,1 +1,16 @@
-#
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <math.h>
+#include <signal.h>
+
+#define Tracking_Right1 0   // BCM_GPIO 17 -> WiringPi 0
+#define Tracking_Right2 7   // BCM_GPIO 4  -> WiringPi 7
+#define Tracking_Left1 2    // BCM_GPIO 27 -> WiringPi 2
+#define Tracking_Left2 3    // BCM_GPIO 22 -> WiringPi 3
+
+#define I2C_ADDR 0x16
